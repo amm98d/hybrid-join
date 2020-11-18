19a6a33ff29fb4b6aa68b3bd25f4ee8138a24111
amm98d <ahmedmustafamalik98@gmail.com>
2020-11-19 00:45:25 +0500

    [Bug Fix] Remove Lib Folder From Git.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,20 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
-		HashTable hashTable = new HashTable(100);
+//		HashTable hashTable = new HashTable(100);
 		DBHandler dbHandler = new DBHandler();
 		ArrayList<Transaction> test = dbHandler.getTransactions(10);
+		System.out.println(test.get(0).TRANSACTION_ID);
+		System.out.println(test.get(test.size()-1).TRANSACTION_ID);
+		System.out.println();
+		test = dbHandler.getTransactions(12);
+		System.out.println(test.get(0).TRANSACTION_ID);
+		System.out.println(test.get(test.size()-1).TRANSACTION_ID);
+		System.out.println();
+		test = dbHandler.getTransactions(10);
+		System.out.println(test.get(0).TRANSACTION_ID);
+		System.out.println(test.get(test.size()-1).TRANSACTION_ID);
+		System.out.println();
 	}
 
 }
